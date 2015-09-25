2079bb41f7adc049242ca31213999c6fb0026cde
Stéphane Lapointe <stephane.lapointe@orckestra.com>
2015-09-25 08:00:00 -0400

    Hello ARM template demo

---
+++ FirstArmTemplateDeployment/Invoke-EmptyTemplateDeployment.ps1
@@ -0,0 +1,5 @@
+﻿Add-AzureAccount
+Switch-AzureMode AzureResourceManager
+
+Select-AzureSubscription -SubscriptionName 'Visual Studio Enterprise with MSDN'
+New-AzureResourceGroup -Name 'Collab365ArmDemos' -DeploymentName 'EmptyTemplateDeployment' -Location 'East US' -TemplateFile "$PSScriptRoot\empty-template.json"
