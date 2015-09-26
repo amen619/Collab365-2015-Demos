cab65b7050514e5ac9f3043025c2a5924165ca98
Stéphane Lapointe <stephane.lapointe@orckestra.com>
2015-09-26 06:53:51 -0400

    Renamed Hello ARM template demo folder and resource group name

--- FirstArmTemplateDeployment/Invoke-EmptyTemplateDeployment.ps1
+++
@@ -1,5 +0,0 @@
-﻿Add-AzureAccount
-Switch-AzureMode AzureResourceManager
-
-Select-AzureSubscription -SubscriptionName 'Visual Studio Enterprise with MSDN'
-New-AzureResourceGroup -Name 'Collab365ArmDemos' -DeploymentName 'EmptyTemplateDeployment' -Location 'East US' -TemplateFile "$PSScriptRoot\empty-template.json"
---
+++ HelloArmTemplate/Invoke-EmptyTemplateDeployment.ps1
@@ -0,0 +1,7 @@
+﻿#Requires -Version 3.0
+
+Add-AzureAccount
+Switch-AzureMode AzureResourceManager
+
+Select-AzureSubscription -SubscriptionName 'Visual Studio Enterprise with MSDN'
+New-AzureResourceGroup -Name 'Collab365-HelloArmTemplate' -DeploymentName 'EmptyTemplateDeployment' -Location 'East US 2' -TemplateFile "$PSScriptRoot\empty-template.json"
