dcff444105a5fbdfd8d498dc4f0fce7f8fe582fe
Stéphane Lapointe <stephane.lapointe@orckestra.com>
2015-09-27 09:34:20 -0400

    updated customization script

--- WindowsVirtualMachine/CustomizeVMScripts/Script.ps1
+++ WindowsVirtualMachine/CustomizeVMScripts/Script.ps1
@@ -4,6 +4,8 @@
 
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install 7zip
+choco install 7zip -confirm -force
 
 New-Item C:\CreatedFromCse -ItemType Directory -Force
+
+exit 0
